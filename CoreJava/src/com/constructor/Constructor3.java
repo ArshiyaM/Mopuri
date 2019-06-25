d67ccc4d1e64c18f86785e7990b8b2eae5a9bf13
rmopuri <rmopuri@AILP44.alten-india.dir>
2019-06-25 16:32:07 +0530

    Constructor3 push again

---
+++ CoreJava/src/com/constructor/Constructor3.java
@@ -0,0 +1,17 @@
+/**
+ * ::: :::: ADVANTEGES OF CONSTRUCTORS:::: :::
+ */
+
+package com.constructor;
+
+public class Constructor3 {
+	
+	public Constructor3() {
+		System.out.println("Constructor Login here");
+	}
+	
+	public static void main(String[] args) {
+		Constructor3 cons3 = new Constructor3();
+	}
+
+}
