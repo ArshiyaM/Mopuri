6d133b727e4f277d42e3dde92bab18731f56a6ae
rmopuri <rmopuri@AILP44.alten-india.dir>
2019-07-08 19:10:02 +0530

    Local Variables learning is in progress....!!!

---
+++ CoreJava/src/com/a/variables/TheroyOnVariables.java
@@ -0,0 +1,41 @@
+/**
+ * Purpose of Variables
+ * ---------------------
+ * Variables are used to store the values by using these values we are achieving the project requirements.
+ * 
+ * Ex: Employee Project contains variables as mentioned below
+ * int empId=111;
+ * String emPname = "MRT";
+ * 
+ * Types of Variables
+ * ------------------
+ * ------------------
+ * 1. Local Variables:
+ * 
+ * i. Variables which are declared inside a method/inside constructor/ inside a blocks(static or instance) are called "Local Variables".
+ *    EX: LocalVariable1
+ * ii. Scope: Scope of the Local Variable is only within the method/within constructor/ within a blocks(static or instance).
+ * iii. Memory Allocation: Local Variables memory allocated when the method is invoked. And also memory is released when the method execution is completed.
+ * 
+ * 
+ * int a=10; --> Here 'a' is variable of type 'int'
+ * Variable Types: 
+ * 
+ * Every variable must be contains with its own type. They are:
+ * a. Primitive type ==> Ex- int a=10; float f=10.5f;
+ * b. class-type     ==> Ex - Test t; Employee emp;
+ * c. Array-type     ==> Ex - int[] a; Staring[] str;
+ *  
+ * 
+ * 
+ * 2. Instance Variables
+ * 3. Static Variables
+ * 
+ * 
+ */
+
+package com.a.variables;
+
+public class TheroyOnVariables {
+
+}
